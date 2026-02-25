3f672f7422c7d4b03a361940b90ede53658f68d6
The-Peppermint-Snake <brkovalik@gmail.com>
2026-02-25 14:07:40 -0500

    idk what im ment to be doing :(

---
+++ script.js
@@ -0,0 +1,11 @@
+let sceneNum = 1;
+
+function advanceScene() {
+    if sceneNum === 1 {
+        //img src updates to "images/forest_path_2"
+        sceneNum++;
+    } else {
+        //img src updates to "images/forest_path_3"
+        sceneNum++;
+    }
+}
