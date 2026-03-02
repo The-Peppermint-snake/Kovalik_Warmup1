939d05bc6b280af1c7a0c102e5f82fa7e18a80f7
The-Peppermint-Snake <brkovalik@gmail.com>
2026-03-02 14:06:13 -0500

    finisehd engouh

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
-let sceneNum = 1;
-
-function advanceScene() {
-    if sceneNum === 1 {
-        //img src updates to "images/forest_path_2"
-        sceneNum++;
-    } else {
-        //img src updates to "images/forest_path_3"
-        sceneNum++;
+const button = document.getElementById("button");
+const img = document.getElementById("img");
+const cap = document.getElementById("caption")
+let sceneNumber = 1;
+button.addEventListener("click", function() {
+    if (sceneNumber == 1) {
+        img.src = "images/forest_path_2.jpg";
+        cap.textContent = "you enter";
+        sceneNumber++;
+    } else if (sceneNumber == 2) {
+        img.src = "images/forest_path_3.jpg";
+        cap.textContent = "What a pretty place";
+        sceneNumber++;
     }
-}
+});
